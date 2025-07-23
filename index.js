51e01b86923fcb0520a01f1236e55d8bab0da4f9
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2025-07-23 07:35:44 +0700

    with mongodb

---
+++ index.js
@@ -0,0 +1,29 @@
+const productRoute = require("./routes/product.route.js");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
